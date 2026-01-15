d0fd8cbd2cae5dfd288646a311b220c0c9a2d12a
mes-petits-delices <tonyhm9416@gmail.com>
2026-01-15 18:35:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-// Header collant au scroll
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-// Menu Mobile
 function toggleMenu(){
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
 
-// Galerie avec noms
 function ouvrirGalerie(type) {
     const modale = document.getElementById('fenetreGalerie');
     const titre = document.getElementById('titreGalerie');
     const grille = document.getElementById('contenuPhotos');
-    
-    grille.innerHTML = ""; 
-    
+    grille.innerHTML = "";
     if (type === 'gateaux') {
         titre.innerText = "Nos Spécialités Sucrées";
-        grille.innerHTML = `
-            <div class="item-galerie"><img src="https://images.pexels.com/photos/1070850/pexels-photo-1070850.jpeg"><p>Gâteau Royal Chocolat</p></div>
-            <div class="item-galerie"><img src="https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg"><p>Tartelette aux Fraises</p></div>
-        `;
+        grille.innerHTML = `<div><img src="https://images.pexels.com/photos/1070850/pexels-photo-1070850.jpeg"><p>Gâteau Royal</p></div><div><img src="https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg"><p>Tarte Fraise</p></div>`;
     } else if (type === 'burgers') {
-        titre.innerText = "Nos Menus Gourmands";
-        grille.innerHTML = `
-            <div class="item-galerie"><img src="https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg"><p>Le Burger Signature</p></div>
-            <div class="item-galerie"><img src="https://images.pexels.com/photos/1199941/pexels-photo-1199941.jpeg"><p>Burger Montagnard</p></div>
-        `;
+        titre.innerText = "Nos Burgers Gourmet";
+        grille.innerHTML = `<div><img src="https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg"><p>Le Classique</p></div><div><img src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg"><p>Le Montagnard</p></div>`;
     }
     modale.style.display = "block";
 }
